e382c69c98564ee4e53c4fa402e52ab10e60b86d
dacortess <dacortess@unal.edu.co>
2026-03-27 20:02:05 -0500

    feat: Spring Skeleton

---
+++ src/test/java/com/isialabs/talentscorer/TalentscorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isialabs.talentscorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentscorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
